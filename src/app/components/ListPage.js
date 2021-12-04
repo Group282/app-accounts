2afe757b1fa8876c29e2058e999c2b20f90f216e
Anastasia21O <virstanastya21@gmail.com>
2021-12-04 16:21:49 +0200

    (fix) new menu

--- src/app/components/ListPage.js
+++ src/app/components/ListPage.js
@@ -9,14 +9,12 @@ import {Link} from "react-router-dom";
 import Alert from "react-bootstrap/Alert";
 import Badge from "react-bootstrap/Badge";
 import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
 import AddUser from "./sections/AddUser";
 import EditUser from "./sections/EditUser";
 
 
 const ListPage = () => {
     const [allUser, setAllUser] = useState([]);
-    const [isLoad, setIsLoad] = useState(false);
     const [showModal, setShowModal] = useState(false)
     const [showModalUpdate, setShowModalUpdate] = useState(false)
     const [idUpdate, setIdUpdate] = useState('')
@@ -29,127 +27,123 @@ const ListPage = () => {
             let expiration_date = new Date(Number.parseInt(item.expiration_date) * 1000)
             item['expiration_date'] = expiration_date.toLocaleDateString()
         }
-        setAllUser(users);
+        setAllUser(users.reverse());
     }
 
     useEffect(async () => {
         await upData()
-        setIsLoad(true);
     }, []);
 
 
     return (
-        !isLoad ?
-            <div>
-                Loading
-            </div> :
-            <Container className="mt-5">
-                <Row>
-                    <Col>
-                        <h2>Account list</h2>
-                    </Col>
-                    <Col className="text-end">
-                        <Button onClick={() => setShowModal(true)} variant="primary">Create account</Button>
-                        <Modal show={showModal}
-                               size="md"
-                               onHide={() => setShowModal(false)}
-                               animation={false}>
-                            <Modal.Header closeButton>
-                                <Modal.Title>Create account</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                                <AddUser fun={
-                                    async () => {
-                                        setShowModal(false)
-                                        await upData()
-                                    }
-                                }/>
-                            </Modal.Body>
-                        </Modal>
-                    </Col>
-                </Row>
-                <Row>
-                    <Alert variant="success">
-                        Total: {allUser.length}
-                    </Alert>
-                </Row>
-                <Row>
-                    <Col className="col-12">
-                        <Table striped bordered hover>
-                            <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Account Name</th>
-                                <th>E-mail</th>
-                                <th>Status</th>
-                                <th>Start Date</th>
-                                <th>Expiration Date</th>
-                                <th>Action</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {
-                                Array.from(allUser).map(item => (
-                                    <tr key={item.id}>
-                                        <td>
-                                            <Link to={"/profile/" + item.id}
-                                                  style={{textDecoration: 'none', color: 'black'}}>
-                                                {item.name}
-                                            </Link>
-                                        </td>
-                                        <td>{item.account_name}</td>
-                                        <td>{item.email}</td>
-                                        <td>
-                                            <Badge bg={
-                                                item.status === "Active" ? "primary" :
-                                                    item.status === "Pending" ? "danger" : "warning"
-                                            }>
-                                                {item.status}
-                                            </Badge>
-                                        </td>
-                                        <td> {item.start_date}</td>
-                                        <td>{item.expiration_date}</td>
-                                        <td>
-                                            <button className="btn btn-primary" onClick={() => {
-                                                setIdUpdate(item.id)
-                                                setShowModalUpdate(true)
-                                            }}>
-                                                Edit
-                                            </button>
-                                            <button className="btn btn-danger"
-                                                    style={{marginLeft: 7}}
-                                                    onClick={async () => {
-                                                        await deleteUser(item.id)
-                                                        await upData()
-                                                    }}
-                                            >
-                                                Delete
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))
-                            }
-                            </tbody>
-                        </Table>
-                    </Col>
-                </Row>
-                <Modal show={showModalUpdate}
-                       size="md"
-                       onHide={() => setShowModalUpdate(false)}
-                       animation={false}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Create account</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <EditUser fun={
-                            async () => {
-                                setShowModalUpdate(false)
-                                await upData()
-                            }
-                        } id={idUpdate}/>
-                    </Modal.Body>
-                </Modal>
-            </Container>
+        <Container className="mt-5">
+            <Row>
+                <Col>
+                    <h2>Account list</h2>
+                </Col>
+                <Col className="text-end">
+                    <Button onClick={() => setShowModal(true)} variant="primary">Create account</Button>
+                    <Modal show={showModal}
+                           size="md"
+                           onHide={() => setShowModal(false)}
+                           animation={false}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Create account</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <AddUser fun={
+                                async () => {
+                                    setShowModal(false)
+                                    await upData()
+                                }
+                            }/>
+                        </Modal.Body>
+                    </Modal>
+                </Col>
+            </Row>
+            <Row>
+                <Alert variant="success">
+                    Total: {allUser.length}
+                </Alert>
+            </Row>
+            <Row>
+                <Col className="col-12">
+                    <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Account Name</th>
+                            <th>E-mail</th>
+                            <th>Status</th>
+                            <th>Start Date</th>
+                            <th>Expiration Date</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            Array.from(allUser).map(item => (
+                                <tr key={item.id}>
+                                    <td>
+                                        <Link to={"/profile/" + item.id}
+                                              style={{textDecoration: 'none', color: 'black'}}>
+                                            {item.name}
+                                        </Link>
+                                    </td>
+                                    <td>{item.account_name}</td>
+                                    <td>{item.email}</td>
+                                    <td>
+                                        <Badge bg={
+                                            item.status === "Active" ? "primary" :
+                                                item.status === "Pending" ? "danger" : "warning"
+                                        }>
+                                            {item.status}
+                                        </Badge>
+                                    </td>
+                                    <td> {item.start_date}</td>
+                                    <td>{item.expiration_date}</td>
+                                    <td>
+                                        <button className="btn btn-primary btn-sm w-50" onClick={() => {
+                                            setIdUpdate(item.id)
+                                            setShowModalUpdate(true)
+                                        }}>
+                                            Edit
+                                        </button>
+
+                                        <button className="btn btn-danger btn-sm w-50"
+
+                                                onClick={async () => {
+                                                    await deleteUser(item.id)
+                                                    await upData()
+                                                }}
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+            <Modal show={showModalUpdate}
+                   size="md"
+                   onHide={() => setShowModalUpdate(false)}
+                   animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Create account</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <EditUser fun={
+                        async () => {
+                            setShowModalUpdate(false)
+                            await upData()
+                        }
+                    } id={idUpdate}/>
+                </Modal.Body>
+            </Modal>
+        </Container>
     )
 }
 export default ListPage;
--- src/app/components/sections/Menu.js
+++ src/app/components/sections/Menu.js
@@ -1,29 +1,57 @@
 import React from "react";
-import {Container, Nav, Button} from 'react-bootstrap'
+import {Container, Nav} from 'react-bootstrap'
 import {Link} from "react-router-dom";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Menu = () => {
   return (
-    <div className="menu bg-black">
-      <Container>
-        <Nav>
-          <Nav.Item>
-            <Nav.Link>
-              <Button variant="success">
-                <Link to="/" style={{textDecoration: 'none', color: 'white'}}>List</Link>
-              </Button>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>
-              <Button variant="success">
-                <Link to="/about" style={{textDecoration: 'none', color: 'white'}}>About Us</Link>
-              </Button>
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
+      <Container fluid className="bg-light navbar-light">
+        <Row>
+          <Col className="col-1"> </Col>
+          <Col className="col-10">
+            <nav className="navbar navbar-expand-lg ">
+              <a className="navbar-brand" href="#">VIRSTAS</a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"/>
+              </button>
+
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item active">
+                    <Link className="nav-link" to="/" style={{textDecoration: 'none', color: 'black'}}>List</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/about" style={{textDecoration: 'none', color: 'black'}}>About Us</Link>
+                  </li>
+                </ul>
+              </div>
+              {/*<div className="menu bg-secondary">*/}
+              {/*  <Container>*/}
+              {/*    <Nav>*/}
+              {/*      <Nav.Item>*/}
+              {/*        <Nav.Link>*/}
+              {/*          <button className="btn btn-info" style={{width: 100}}>*/}
+              {/*
+          {/*          </button>*/}
+              {/*        </Nav.Link>*/}
+              {/*      </Nav.Item>*/}
+              {/*      <Nav.Item>*/}
+              {/*        <Nav.Link>*/}
+              {/*          <button className="btn btn-info" style={{width: 100}}>*/}
+              {/*            */}
+              {/*          </button>*/}
+              {/*        </Nav.Link>*/}
+              {/*      </Nav.Item>*/}
+              {/*    </Nav>*/}
+              {/*  </Container>*/}
+              {/*</div>*/}
+            </nav>
+          </Col>
+          <Col className="col-1"> </Col>
+        </Row>
       </Container>
-    </div>
   )
 };
 
